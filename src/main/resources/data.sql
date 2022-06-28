6a1eecc9b40fd4dc9306e2ad4c51c764d055bc84
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-06-28 15:08:02 +0200

    Added base admin user initialisation and changed db to reset after app is stopped;

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
-INSERT INTO roles (id, name) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO roles (id, name) VALUES (2, 'ROLE_USER');
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (name) VALUES ('ROLE_USER');
+INSERT INTO users (username, password, email)
+    VALUES ('admin', '$2a$10$LNYZhv4c3OywYPXHZXd.iOIjWi4XfDipg4tG65uv7wbIelftWq.ie', 'admin@admin.org');  --passwd: admin
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 2);
