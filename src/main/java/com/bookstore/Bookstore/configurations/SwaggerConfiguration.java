df50ac0d645733b74eb453ad0b6f7017d6a70280
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-06-28 12:29:11 +0200

    Fixed project using wrong swagger config file;

---
+++ src/main/java/com/bookstore/Bookstore/configurations/SwaggerConfiguration.java
@@ -0,0 +1,43 @@
+package com.bookstore.Bookstore.configurations;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public GroupedOpenApi apiGroup() {
+        return GroupedOpenApi
+                .builder()
+                .group("Api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI apiInfo() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")))
+                .info(
+                        new Info()
+                                .title("Bookstore API")
+                                .description("REST API for a bookstore")
+                                .version("1.0"));
+    }
+}
