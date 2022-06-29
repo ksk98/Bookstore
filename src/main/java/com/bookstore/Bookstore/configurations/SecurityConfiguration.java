a1828ed7c7aeb815d96921728bb85c73aaa30409
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-06-29 10:36:17 +0200

    Fixed @PreAuthorize not working;

--- src/main/java/com/bookstore/Bookstore/configurations/SecurityConfiguration.java
+++ src/main/java/com/bookstore/Bookstore/configurations/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +21,7 @@ import static java.lang.String.format;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final JwtFilter filter;
     private final UserDetailsService userDetailsService;
