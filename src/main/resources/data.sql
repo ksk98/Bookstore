e81b11e740d8a70db92f6fa379a4cf036859eb91
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-06-29 14:55:39 +0200

    Books are now initialised;

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,12 @@
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
 INSERT INTO roles (name) VALUES ('ROLE_USER');
-INSERT INTO users (username, password, email)
-    VALUES ('admin', '$2a$10$LNYZhv4c3OywYPXHZXd.iOIjWi4XfDipg4tG65uv7wbIelftWq.ie', 'admin@admin.org');  --passwd: admin
+
+INSERT INTO users (username, password, email) VALUES
+    ('admin', '$2a$10$LNYZhv4c3OywYPXHZXd.iOIjWi4XfDipg4tG65uv7wbIelftWq.ie', 'admin@admin.org');  --passwd: admin
+
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 2);
+
+INSERT INTO books (title, author, publisher) VALUES
+    ('Metro 2035', 'Dimitry Glukhovsky', 'Insignis Media'),
+    ('Wyspa Złoczyńców', 'Zbigniew Nienacki', 'Pojezierze')
